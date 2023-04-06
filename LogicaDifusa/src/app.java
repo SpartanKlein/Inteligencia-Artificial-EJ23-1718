e584f872ebd788ef3e1c98dc67f661c483239262
Jaime Alonso Ruiz Lizarraga <109649412+SpartanKlein@users.noreply.github.com>
2023-04-05 22:36:19 -0600

    Proyecto Logica Difusa
    Proyecto de inteligencia Artificial de Lógica Difusa utilizando JFuzzyLogic

---
+++ LogicaDifusa/src/app.java
@@ -0,0 +1,67 @@
+
+import java.util.Scanner;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+
+public class app {
+	static Scanner Leer = new Scanner(System.in);
+	static  FIS fis;
+
+	public static void main(String[] args) {
+		 fis = FIS.load("C:\\TecNM Campus Culiacán\\Semestre 8\\Inteligencia Artificial\\LogicaDifusa\\src\\potenciaTiro.fcl",true);
+
+		int resultado = 0;
+		do {
+			Menu(fis);
+			System.out.println("Quieres repetir el programa? (no -1)");
+			resultado = Leer.nextInt();
+		}while(resultado != -1);
+		Leer.close();
+	}
+	static public void Menu(FIS fis) {
+		int fuerzaValor = 0;
+		int anguloValor = 0;
+		boolean bandera;
+
+		System.out.println("Describa como tiro el jugador el balon");
+		do{
+			bandera = true;
+			System.out.println("Con Cuanta Fuerza Se Lanzo el balon (poca/masomenos/mucha)");
+			String estadoFuerza = Leer.next();
+			switch (estadoFuerza){
+				case "poca": fuerzaValor = 1 + (int)(Math.random() *3); break;
+				case "masomenos": fuerzaValor = 3 + (int)(Math.random() * 6); break;
+				case "mucha": fuerzaValor = 9 + (int)(Math.random() * 3); break;
+
+				default: System.out.println("opcion no valida");  bandera= false;
+			}
+		}while(!bandera);
+		do{
+			bandera = true;
+			System.out.println("Como fue el angulo que se Lanzo el balon (bajo/medio/alto)");
+			String estadoAngulo = Leer.next();
+			switch (estadoAngulo){
+				case "bajo":anguloValor = 1 + (int)(Math.random() * 3);  break;
+				case "medio":anguloValor = 3 + (int)(Math.random() *6);  break;
+				case "alto":anguloValor = 9 + (int)(Math.random() * 3);  break;
+
+				default: System.out.println("opcion no valida"); bandera= false;
+			}
+		}while(!bandera);
+		//Aplicar fuzzyLogic
+		fis.setVariable("fuerza",fuerzaValor);
+		fis.setVariable("angulo",anguloValor);
+		fis.evaluate();
+
+		double distanciaBalon = fis.getVariable("distancia").getValue();
+		System.out.println("distancia Recorrida:" + distanciaBalon );
+		if(distanciaBalon < 5 ){
+			System.out.println("el balon no llego");
+		}else if(distanciaBalon >= 5 && distanciaBalon  <= 7){
+			System.out.println("se ensesto el balon");
+		}else{
+			System.out.println("el balon se paso al aro");
+		}
+
+	}
+}
